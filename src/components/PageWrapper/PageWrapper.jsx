e6d1dc175388998a49a17136f544ee331c1ead1c
Callum Harrod <callumharrod1994@hotmail.co.uk>
2026-03-16 14:17:56 +0000

    Tried adding a space

--- src/components/PageWrapper/PageWrapper.jsx
+++ src/components/PageWrapper/PageWrapper.jsx
@@ -6,10 +6,10 @@ export default function PageWrapper({ children }) {
   const isIdeas = pathname.startsWith("/ideas");
   return (
     <div
-      className={`flex w-full max-w-[1440px] flex-col mx-auto relative z-10${isIdeas ? "" : " pb-20 sm:pb-[160px]"}`}
+      className={`flex w-full max-w-[1440px] flex-col mx-auto relative z-10 ${isIdeas ? "" : " pb-20 sm:pb-[160px]"}`}
     >
       <div
-        className={`mx-5 sm:mx-10 flex flex-col pt-8${isIdeas ? " gap-4" : " gap-30 sm:gap-[240px]"}`}
+        className={`mx-5 sm:mx-10 flex flex-col pt-8 ${isIdeas ? " gap-4" : " gap-30 sm:gap-[240px]"}`}
       >
         {children}
       </div>
